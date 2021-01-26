0d02bab9139aa9989a3edd62bdda3f1780e94c61
TheArrayOfThings <ryan.flanagan447@gmail.com>
2021-01-26 14:42:15 +0000

    Update Logo.js

--- js/Logo.js
+++ js/Logo.js
@@ -1,4 +1,4 @@
-/*var c = document.getElementById("topLogo");
+var c = document.getElementById("topLogo");
 var ctx = c.getContext("2d");
 var grad = ctx.createLinearGradient(0,0,140,0);
 grad.addColorStop(0,"orange");
@@ -9,4 +9,4 @@ ctx.fillRect(0,0,140,38);
 
 ctx.fillStyle = "black";
 ctx.font = "italic bold 22px calibri";
-ctx.fillText("Computering",12,25);*/
+ctx.fillText("Computering",12,25);
