b532c928185a0af755c624479b810118094d3ffc
TheArrayOfThings <ryan.flanagan447@gmail.com>
2021-01-26 14:53:40 +0000

    Still fixing logo

--- js/Logo.js
+++ js/Logo.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function () {
+function drawLogo() {
 	var c = document.getElementById("topLogo");
 	var ctx = c.getContext("2d");
 	var grad = ctx.createLinearGradient(0,0,140,0);
@@ -11,4 +11,4 @@ window.addEventListener('load', function () {
 	ctx.fillStyle = "black";
 	ctx.font = "italic bold 22px calibri";
 	ctx.fillText("Computering",12,25);
-})
+}
