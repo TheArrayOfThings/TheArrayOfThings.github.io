966043a3d7ac0ea3aa1c71efa08a3d2448634e24
TheArrayOfThings <ryan.flanagan447@gmail.com>
2021-01-27 17:26:49 +0000

    Logo Fixes

--- js/Logo.js
+++ js/Logo.js
@@ -1,15 +1,21 @@
 function drawLogo() {
 	var c = document.getElementById("topLogo");
+	c.height = c.clientHeight;
+	c.width = c.clientWidth;
 	var ctx = c.getContext("2d");
 	var grad = ctx.createLinearGradient(0,0,140,0);
 	grad.addColorStop(0,"orange");
 	grad.addColorStop(1,"#f44e42");
 
 	ctx.fillStyle = grad;
-	ctx.fillRect(0,0,c.clientWidth,c.clientHeight);
+	ctx.fillRect(0,0,c.width,c.height);
 
 	ctx.fillStyle = "black";
-	ctx.font = "italic bold 22px calibri";
-	ctx.fillText("Ryan Flanagan",c.clientWidth/3.5,c.clientHeight*0.5);
+	ctx.font = "bold 22px calibri";
+	ctx.fillText("Ryan Flanagan",c.width/10,c.height*0.75);
+	console.log("Logo drawn!");
 }
 drawLogo();
+window.onresize = function(event) {
+	drawLogo();
+};
