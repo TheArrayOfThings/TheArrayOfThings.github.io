abb4ce54b13826e51e3c4fead489404a340f97ba
TheArrayOfThings <ryan.flanagan447@gmail.com>
2021-02-03 10:40:23 +0000

    Update Logo.js

--- js/Logo.js
+++ js/Logo.js
@@ -1,21 +1,25 @@
 function drawLogo() {
-	var canvas = document.getElementById("topLogo");
-	var container = document.getElementsByClassName("logoContainer")[0];
-	canvas.height = container.clientHeight;
-	canvas.width = container.clientWidth;
-	var ctx = canvas.getContext("2d");
-	var grad = ctx.createLinearGradient(0,0,140,0);
-	grad.addColorStop(0,"orange");
-	grad.addColorStop(1,"#f44e42");
+	try {
+		var canvas = document.getElementById("topLogo");
+		var container = document.getElementsByClassName("logoContainer")[0];
+		canvas.height = container.clientHeight;
+		canvas.width = container.clientWidth;
+		var ctx = canvas.getContext("2d");
+		var grad = ctx.createLinearGradient(0,0,140,0);
+		grad.addColorStop(0,"orange");
+		grad.addColorStop(1,"#f44e42");
 
-	ctx.fillStyle = grad;
-	ctx.fillRect(0,0,canvas.width,canvas.height);
+		ctx.fillStyle = grad;
+		ctx.fillRect(0,0,canvas.width,canvas.height);
 
-	ctx.fillStyle = "black";
-	ctx.font = "bold " + (canvas.height/2) + "px calibri";
-	ctx.textAlign = "center";
-	ctx.fillText("The Array Of Things", canvas.width/2, canvas.height/1.5);
-	console.log("Success!");
+		ctx.fillStyle = "black";
+		ctx.font = "bold " + (canvas.height/2) + "px calibri";
+		ctx.textAlign = "center";
+		ctx.fillText("The Array Of Things", canvas.width/2, canvas.height/1.5);
+		console.log("Success!");
+	} catch (err) {
+		setTimeout(drawLogo, 10);
+	}
 }
 
 //Draw the logo after finished resizing
@@ -42,13 +46,4 @@ function resizeend() {
 
 //I think jQuery is overwriting the onload function. So I'm just going to keep trying to redraw the logo until it works.
 
-function tryDrawLogo() {
-	try {
-		console.log("Trying to draw the logo...");
-		drawLogo();
-	} catch (error) {
-		setTimeout(drawLogo, 10);
-	}
-}
-
-tryDrawLogo();
+drawLogo();
