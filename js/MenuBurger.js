c22a1566970ea4ea5f303060772ae4070ee0ac95
TheArrayOfThings <ryan.flanagan447@gmail.com>
2021-02-11 16:07:52 +0000

    Menu functionality

---
+++ js/MenuBurger.js
@@ -0,0 +1,21 @@
+var menu;
+var visible = false;
+try {
+	attachEvent();
+}
+catch {
+	setTimeout(attachEvent, 10);
+}
+
+function attachEvent() {
+	menu = document.getElementsByClassName("navContainer")[0];
+	document.getElementsByClassName("topMenuRight")[0].onclick = function () {
+		if (visible) {
+			visible = false;
+			menu.style.display = "none";
+		} else {
+			visible = true;
+			menu.style.display = "unset";
+		}
+	};
+}
