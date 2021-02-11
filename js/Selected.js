10f9e27cfd3763468ce11193749d82f3091cc80a
TheArrayOfThings <ryan.flanagan447@gmail.com>
2021-02-11 16:32:34 +0000

    Update Selected.js

--- js/Selected.js
+++ js/Selected.js
@@ -1,7 +1,9 @@
 function setSelected() {
 	var navigation = document.getElementsByClassName('navigate');
 	for (var i = 0; i < navigation.length; ++i) {
-		if (navigation[i].href == window.location.href || navigation[i].href == "https://thearrayofthings.github.io/index.html" && window.location.href == "https://thearrayofthings.github.io/") {
+		if (navigation[i].href == window.location.href) {
+			navigation[i].className = 'current';
+		} else if (navigation[i].href == "https://thearrayofthings.github.io/index.html" && window.location.href == "https://thearrayofthings.github.io/") {
 			navigation[i].className = 'current';
 		}
 	}
