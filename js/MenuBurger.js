cc1e0dfee148679e3b8a6e2a676bd15d3a7369e7
TheArrayOfThings <ryan.flanagan447@gmail.com>
2021-02-11 16:16:47 +0000

    Update MenuBurger.js

--- js/MenuBurger.js
+++ js/MenuBurger.js
@@ -1,25 +1,21 @@
-function startupFunction () {
+function attachEvent () {
 	var menu;
 	var visible = false;
 	try {
-		attachEvent();
+		menu = document.getElementsByClassName("navContainer")[0];
+		document.getElementsByClassName("topMenuRight")[0].onclick = function () {
+			if (visible) {
+				visible = false;
+				menu.style.display = "none";
+			} else {
+				visible = true;
+				menu.style.display = "unset";
+			}
+		}
 	}
 	catch (err) {
 		setTimeout(attachEvent, 10);
 	}
 }
 
-function attachEvent() {
-	menu = document.getElementsByClassName("navContainer")[0];
-	document.getElementsByClassName("topMenuRight")[0].onclick = function () {
-		if (visible) {
-			visible = false;
-			menu.style.display = "none";
-		} else {
-			visible = true;
-			menu.style.display = "unset";
-		}
-	};
-}
-
-startupFunction();
+attachEvent();
