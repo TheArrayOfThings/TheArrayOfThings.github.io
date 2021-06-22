b73b90e445907f8b0d890c8350aa02fae4a20cba
TheArrayOfThings <36077267+TheArrayOfThings@users.noreply.github.com>
2021-06-22 11:54:11 +0100

    Dartmoor continued

--- Dartmoor/js/Dartmoor.js
+++ Dartmoor/js/Dartmoor.js
@@ -1,14 +1,12 @@
-<script>
-    var map;
+var map;
 
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 3,
-            center: {
-                lat: 39.8283,
-                lng: -98.5795
-            }
-        });
-    }
-</script>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD82Y97n7A_rouMcx2clbLCH2TifSEWfgU&callback=initMap" type="text/javascript"></script>
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 3,
+		center: {
+			lat: 39.8283,
+			lng: -98.5795
+		}
+	});
+}
+initMap();
