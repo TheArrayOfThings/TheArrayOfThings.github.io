72054f5cef634371555882dc1c11017ab618210a
TheArrayOfThings <36077267+TheArrayOfThings@users.noreply.github.com>
2021-06-22 12:34:11 +0100

    Dartmoor fixes

--- Dartmoor/js/Dartmoor.js
+++ Dartmoor/js/Dartmoor.js
@@ -1,12 +1,22 @@
 var map;
 
+window.onload = initMap;
+
 function initMap() {
+	console.log("Starting to render map...");
 	map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 3,
+		zoom: 10,
 		center: {
-			lat: 39.8283,
-			lng: -98.5795
+			lat: 50.574043351632646,
+			lng: -3.925963623267205
 		}
 	});
+	map.data.loadGeoJson('/Dartmoor/Resources/camping_webmap.json');
+	
+	map.data.setStyle({
+        fillColor: 'green',
+        strokeWeight: 1
+    });
 }
-initMap();
+
+//50.574043351632646, -3.925963623267205
