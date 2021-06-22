4f6f3c7691c835bf22946d8babf19698f0b125fc
TheArrayOfThings <36077267+TheArrayOfThings@users.noreply.github.com>
2021-06-22 15:41:29 +0100

    Map style changes

--- Dartmoor/js/Dartmoor.js
+++ Dartmoor/js/Dartmoor.js
@@ -1,5 +1,5 @@
 function initMap() {
-	var map;
+	let map, infoWindow;
 	console.log("Starting to render map...");
 	map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 10,
@@ -8,13 +8,49 @@ function initMap() {
 			lng: -3.925963623267205
 		}
 	});
-	
 	map.data.setStyle({
-        fillColor: 'green',
-        strokeWeight: 1
+        fillColor: 'orange',
+        strokeWeight: 1,
+		fillOpacity: 0.5
     });
-	console.log("Attempting to load geoJSON 3");
 	map.data.loadGeoJson('https://thearrayofthings.github.io/Dartmoor/Resources/camping_webmap.geojson');
 	map.data.setMap(map);
-	console.log(map);
+	infoWindow = new google.maps.InfoWindow();
+	const locationButton = document.createElement("button");
+	locationButton.textContent = "Pan to Current Location";
+	locationButton.classList.add("custom-map-control-button");
+	map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+	locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
