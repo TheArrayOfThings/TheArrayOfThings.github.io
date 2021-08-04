b171d29be8c76d5865d61bd20b06e915da8d8044
TheArrayOfThings <36077267+TheArrayOfThings@users.noreply.github.com>
2021-08-04 19:16:24 +0100

    More Visual Changes + logo overhaul

--- js/Logo.js
+++ js/Logo.js
@@ -1,40 +1,29 @@
-function drawLogo() {
-	var fontSizeMaxAttempts = 200;
+function resizeLogoText() {
 	try {
-		//First, create the canvas
-		var canvas = document.getElementById("topLogo");
-		var container = document.getElementsByClassName("logoContainer")[0];
-		canvas.width = container.clientWidth;
-		canvas.height = container.clientHeight;
+		console.log("Started...");
+		var logoText = document.getElementsByClassName("topLogoTextContainer")[0];
 		var fontScale = 1;
-		var fontScaleMaxTries = 50;
-		var currentFontSize = 4;
-		var ctx = canvas.getContext("2d");
-		var grad = ctx.createLinearGradient(0,0,140,0);
-		grad.addColorStop(0,"#A239CA");
-		grad.addColorStop(1,"#4717F6");
-		ctx.fillStyle = grad;
-		ctx.fillRect(0,0,canvas.width,canvas.height);
-		var logoText = document.getElementsByClassName("topLogoText")[0];
-		logoText.style.font = "bold " + currentFontSize + "vw calibri";
-		//Second, change the text depending on whether the screen is landscape or portrait
-		//console.log("Inner height: " + window.innerHeight + ", inner width: " + window.innerWidth);
+		var fontScaleMaxTries = 1000;
+		var currentFontSize = 1;
+		var fontStep = 0.2;
+		var container = document.getElementsByClassName("logoContainer")[0];
 		if(window.innerHeight > window.innerWidth){ 	//Portrait mode
 			logoText.innerHTML = "ARRAY<br>OF<br>THINGS";
 		} else {	//Landscape mode
 			logoText.innerHTML = "ARRAY OF THINGS";
 		}
-		//Third scale it as big as I can without overflow
+		//Scale it as big as I can without overflow
 		for (var i = 0; i < fontScaleMaxTries; ++i) {
 			logoText.style.fontSize = (currentFontSize * fontScale) + "px";
+				console.log("Text width: " + logoText.clientWidth + ", canvas width: " + container.clientWidth + ", text height: " + logoText.clientHeight + ", canvas height: " + container.clientHeight + ", final font size: " + logoText.style.fontSize);
 			if (logoText.clientWidth >= container.clientWidth - (container.clientWidth*0.05) || logoText.clientHeight >= container.clientHeight - (container.clientHeight*0.05)) {
-				logoText.style.fontSize = (currentFontSize * (fontScale - 0.1)) + "px";
-				//console.log("Text width: " + logoText.clientWidth + ", canvas width: " + container.clientWidth + ", text height: " + logoText.clientHeight + ", canvas height: " + container.clientHeight + ", final font size: " + logoText.style.fontSize);
+				logoText.style.fontSize = (currentFontSize * (fontScale - fontStep)) + "px";
+				console.log("Dying...");
 				break;
 			}
-			fontScale = fontScale + 0.1;
+			fontScale = fontScale + fontStep;
 		}
-		//Lastly, sort out the canvas text (using a separate Div, as there's more manipulation we can do!)
+		//Now centre the text div
 		logoText.style.left = (container.clientWidth/2 - logoText.clientWidth/2)  + "px";
 		logoText.style.top = (container.clientHeight/2 - logoText.clientHeight/2)  + "px";
 	} catch (err) {
@@ -61,7 +50,7 @@ function getFontHeight() {
 
 var logoResizeRtime;
 var logoResizeTimeout = false;
-var logoResizeDelta = 10;
+var logoResizeDelta = 100;
 
 window.addEventListener("resize", function() {
     logoResizeRtime = new Date();
@@ -76,10 +65,6 @@ function logoResizeend() {
         setTimeout(logoResizeend, logoResizeDelta);
     } else {
         logoResizeTimeout = false;
-        drawLogo();
+        resizeLogoText();
     }               
 }
-
-//I think jQuery is overwriting the onload function. So I'm just going to keep trying to redraw the logo until it works.
-
-//drawLogo();
