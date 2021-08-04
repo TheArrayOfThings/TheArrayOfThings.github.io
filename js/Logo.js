07c162b4f2d76c6f72f8c39ed480483ea19b98f0
TheArrayOfThings <36077267+TheArrayOfThings@users.noreply.github.com>
2021-08-04 14:30:47 +0100

    Visual changes

--- js/Logo.js
+++ js/Logo.js
@@ -1,71 +1,44 @@
-var currentFontSize = 5;
 function drawLogo() {
 	var fontSizeMaxAttempts = 200;
 	try {
+		//First, create the canvas
 		var canvas = document.getElementById("topLogo");
 		var container = document.getElementsByClassName("logoContainer")[0];
 		canvas.width = container.clientWidth;
 		canvas.height = container.clientHeight;
-		var startFontSize = 5;
-		var fontSizeStep = 0.5;
-		currentFontSize = 5;
+		var fontScale = 1;
+		var fontScaleMaxTries = 50;
+		var currentFontSize = 4;
 		var ctx = canvas.getContext("2d");
-		if(window.innerHeight > window.innerWidth){ //Portrait mode
-			var grad = ctx.createLinearGradient(0,0,140,0);
-			grad.addColorStop(0,"#A239CA");
-			grad.addColorStop(1,"#4717F6");
-
-			ctx.fillStyle = grad;
-			ctx.fillRect(0,0,canvas.width,canvas.height);
-
-			ctx.fillStyle = "white";
-			ctx.textAlign = "center";
-			ctx.font = "bold " + startFontSize + "vw calibri";
-			for (var i = 0; i < fontSizeMaxAttempts; ++i) {
-				console.log("Loop");
-				if (ctx.measureText("THINGS").width > canvas.clientWidth) {
-					currentFontSize = currentFontSize - fontSizeStep;
-					ctx.font = "bold " + currentFontSize + "vw calibri";
-				} else {
-					break;
-				}
-			}
-			for (var i = 0; i < fontSizeMaxAttempts; ++i) {
-				console.log("Loop");
-				if (getFontHeight()*3 > canvas.clientHeight) {
-					currentFontSize = currentFontSize - fontSizeStep;
-					ctx.font = "bold " + currentFontSize + "vw calibri";
-				} else {
-					break;
-				}
-			}
-			ctx.fillText("ARRAY", (canvas.width/2), canvas.height/3.5);
-			ctx.fillText("OF", (canvas.width/2), canvas.height/1.7);
-			ctx.fillText("THINGS", (canvas.width/2), canvas.height/1.1);
-		} else { //Landscape mode
-			var grad = ctx.createLinearGradient(0,0,140,0);
-			grad.addColorStop(0,"#A239CA");
-			grad.addColorStop(1,"#4717F6");
-
-			ctx.fillStyle = grad;
-			ctx.fillRect(0,0,canvas.width,canvas.height);
-
-			ctx.fillStyle = "white";
-			ctx.textAlign = "center";
-			ctx.font = "bold " + startFontSize + "vw calibri";
-			for (var i = 0; i < fontSizeMaxAttempts; ++i) {
-				if (ctx.measureText("ARRAY OF THINGS").width > canvas.clientWidth) {
-					currentFontSize = currentFontSize - fontSizeStep;
-					ctx.font = "bold " + currentFontSize + "vw calibri";
-				} else {
-					break;
-				}
-
-			}
-			ctx.fillText("ARRAY OF THINGS", (canvas.width/2), canvas.height/1.5);
+		var grad = ctx.createLinearGradient(0,0,140,0);
+		grad.addColorStop(0,"#A239CA");
+		grad.addColorStop(1,"#4717F6");
+		ctx.fillStyle = grad;
+		ctx.fillRect(0,0,canvas.width,canvas.height);
+		var logoText = document.getElementsByClassName("topLogoText")[0];
+		logoText.style.font = "bold " + currentFontSize + "vw calibri";
+		//Second, change the text depending on whether the screen is landscape or portrait
+		//console.log("Inner height: " + window.innerHeight + ", inner width: " + window.innerWidth);
+		if(window.innerHeight > window.innerWidth){ 	//Portrait mode
+			logoText.innerHTML = "ARRAY<br>OF<br>THINGS";
+		} else {	//Landscape mode
+			logoText.innerHTML = "ARRAY OF THINGS";
 		}
+		//Third scale it as big as I can without overflow
+		for (var i = 0; i < fontScaleMaxTries; ++i) {
+			logoText.style.fontSize = (currentFontSize * fontScale) + "px";
+			if (logoText.clientWidth >= container.clientWidth - (container.clientWidth*0.05) || logoText.clientHeight >= container.clientHeight - (container.clientHeight*0.05)) {
+				logoText.style.fontSize = (currentFontSize * (fontScale - 0.1)) + "px";
+				//console.log("Text width: " + logoText.clientWidth + ", canvas width: " + container.clientWidth + ", text height: " + logoText.clientHeight + ", canvas height: " + container.clientHeight + ", final font size: " + logoText.style.fontSize);
+				break;
+			}
+			fontScale = fontScale + 0.1;
+		}
+		//Lastly, sort out the canvas text (using a separate Div, as there's more manipulation we can do!)
+		logoText.style.left = (container.clientWidth/2 - logoText.clientWidth/2)  + "px";
+		logoText.style.top = (container.clientHeight/2 - logoText.clientHeight/2)  + "px";
 	} catch (err) {
-		setTimeout(drawLogo, 10);
+		console.log(err);
 	}
 }
 
@@ -88,7 +61,7 @@ function getFontHeight() {
 
 var logoResizeRtime;
 var logoResizeTimeout = false;
-var logoResizeDelta = 200;
+var logoResizeDelta = 10;
 
 window.addEventListener("resize", function() {
     logoResizeRtime = new Date();
@@ -109,4 +82,4 @@ function logoResizeend() {
 
 //I think jQuery is overwriting the onload function. So I'm just going to keep trying to redraw the logo until it works.
 
-drawLogo();
+//drawLogo();
