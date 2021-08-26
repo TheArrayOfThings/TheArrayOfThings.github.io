9a7dbbc96e06234e1e2e8dd8b2d273ea29ad1f4d
TheArrayOfThings <36077267+TheArrayOfThings@users.noreply.github.com>
2021-08-26 12:35:56 +0100

    Fixed the page highlighting when using expandable sections

--- js/Selected.js
+++ js/Selected.js
@@ -1,15 +1,21 @@
 function setSelected() {
+	let interalLinkRegex = /#\w*$/g;
+	let windowURL = window.location.href;
+	if (windowURL.search(interalLinkRegex) != -1) {
+		windowURL = windowURL.split(interalLinkRegex)[0];
+		alert(windowURL);
+	}
 	let navigation = document.getElementsByClassName('navigate');
 	for (let i = 0; i < navigation.length; ++i) {
-		if (navigation[i].href == window.location.href) {
+		if (navigation[i].href == windowURL) {
 			navigation[i].className = 'current';
-		} else if (navigation[i].href == "https://thearrayofthings.github.io/index.html" && window.location.href == "https://thearrayofthings.github.io/") {
+		} else if (navigation[i].href == "https://thearrayofthings.github.io/index.html" && windowURL == "https://thearrayofthings.github.io/") {
 			navigation[i].className = 'current';
 		}
 	}
 	let topMenu = document.getElementsByClassName('topMenuLink');
 	for (let i = 0; i < topMenu.length; ++i) {
-		if (topMenu[i].href == window.location.href) {
+		if (topMenu[i].href == windowURL) {
 			topMenu[i].className = 'topMenuCurrent';
 		}
 	}
