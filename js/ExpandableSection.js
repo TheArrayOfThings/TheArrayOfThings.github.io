db803554c1eb6c9d66ca0d7dfd18a95fba91acd8
TheArrayOfThings <ryan.flanagan447@gmail.com>
2023-09-25 18:37:13 +0100

    Bug fixes and content changes

--- js/ExpandableSection.js
+++ js/ExpandableSection.js
@@ -1,45 +1,22 @@
 function initialiseExpandableSection() {
-	if (typeof isInternetExplorer == "undefined" || typeof internalURL == "undefined") {
-		console.log("Retrying...");
+	if (typeof isInternetExplorer == "undefined") {
+		console.log("Loading...");
 		setTimeout(initialiseExpandableSection, 10);
 		return;
 	}
 	let expandableSetions = document.getElementsByClassName("expandableSection");
-	let contentMenu = document.getElementsByClassName("contentMenu")[0];
 	let tempHeader;
-	let tempListItem;
-	let tempLink;
 	for (let e = 0; e < expandableSetions.length; ++e) {
+		console.log(`e: ${e}`);
 		//Get the header used in the expandable section
 		tempHeader = expandableSetions[e].getElementsByClassName("expandableHeader")[0];
 		//Add the event listener to expand the section when clicked
-		addEvent("click", tempHeader, toggleSection);
+		addEvent("click", tempHeader, () => {toggleSection(expandableSetions[e])});
 		//Add a class to the expandable section, so we can easily find it later.
 		expandableSetions[e].classList += " " + parseInternalLink(tempHeader.innerText);
-		//Create the list item for the content menu
-		tempListItem = document.createElement("li");
-		//Create the link used in said list item
-		//Add the custom bullet point replacement
-		tempListItem.innerText = "»";
-		tempLink = document.createElement("a");
-		//Add the class to the link
-		tempLink.classList += "contentLink";
-		//Set the link text to the header text, for consistency
-		tempLink.innerHTML = tempHeader.innerText;
-		//I want the internal links to appear to append to the end of the url, even if I'm not really using the functionality as intended
-		tempLink.href = "#" + parseInternalLink(tempHeader.innerText);
-		//You might be asking yourself "why is he doing something so convoluted here with the anchor redirect? Shouldn't the browser redirect internally without needing all this?"
-		//Well, to that I say that internal links are broken in the current version of FireFox mobile - so I'm overwriting the default behaviour.
-		//Also, I guess this allows me to use smooth scrollIntoView? That's a more valid reason to do this I suppose!
-		tempLink.onclick = function() {
-			let expandableContent = expandableSetions[e].getElementsByClassName("expandableContent")[0];
-			expandableContent.style.display ="block";
-			expandableContent.scrollIntoView({behavior: "smooth"}); 
-		};
-		tempListItem.appendChild(tempLink);
-		contentMenu.appendChild(tempListItem);
 		//Now, we can try and open/navigate to the relevant section on page load, if the URL points to a particular section!
-		if (baseURL != internalURL) {
+		let internalURL = window.location.hash.substring(1);
+		if (internalURL) {
 			if (parseInternalLink(tempHeader.innerText) == internalURL) {
 				//The internal URL in the link is one of our expandable sections! Now open and scroll to it
 				let expandableContent = expandableSetions[e].getElementsByClassName("expandableContent")[0];
@@ -57,9 +34,23 @@ function openContent (classNameToFind) {
 	sectionContent.scrollIntoView({behavior: "smooth"}); 
 }
 
-function toggleSection (sectionContentParam) {
-	console.log(typeof sectionContentParam);
-	let expandableSectionContent = document.getElementsByClassName("expandableContent");
+function toggleSection (section) {
+	console.log(`Toggling section for:`);
+	console.log(section);
+	let expandableSectionContent = section.getElementsByClassName("expandableContent");
+	for (let i = 0; i < expandableSectionContent.length; ++i) {
+		if(expandableSectionContent[i].style.display == "block") {
+			expandableSectionContent[i].style.display = "none";
+			this.href = "#";
+		} else {
+			expandableSectionContent[i].style.display = "block";
+			expandableSectionContent[i].scrollIntoView({behavior: "smooth"}); 
+			let internalLink = section.getElementsByClassName("expandableHeader")[0].id;
+			this.href= `#${internalLink}`;
+		}
+	}
+	return;
+	//let expandableSectionContent = document.getElementsByClassName("expandableContent");
 	let thisSectionContent;
 	if (typeof sectionContentParam != "string") {
 		thisSectionContent = this.parentElement.getElementsByClassName("expandableContent")[0];
@@ -92,10 +83,7 @@ function toggleSection (sectionContentParam) {
 	}
 }
 
-function parseInternalLink(internalLinkToParse) {
-	let returnString = internalLinkToParse.replaceAll(" ", "");
-	returnString = returnString.replaceAll("?", "");
-	return returnString;
-}
 
-addEvent("load", window, initialiseExpandableSection);
+$(document).ready(() => {
+	initialiseExpandableSection();
+});
